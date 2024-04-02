8c6f1a170096295406cd0950cb016cd25e15b613
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-04-02 15:55:37 -0300

    ensures the latest state value is being used

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import TodoList from './components/TodoList';
-import NewTodo from './components/NewTodo';
-import { useState } from 'react';
-import { Todo } from './components/todo.model'
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { useState } from "react";
+import { Todo } from "./todo.model";
 
 function App() {
-  const [todos, setTodos] = useState<{id: string, text: string}[]>([]);
-  
+  const [todos, setTodos] = useState<{ id: string; text: string }[]>([]);
+
   const addHandler = (text: string) => {
-    console.log('text', text)
-    let newTodo = todos;
-    newTodo = [...todos, { id: Math.random().toString(), text: text }]
-    setTodos(newTodo);
-  }
-  
+    console.log("text", text);
+    setTodos((previousTodosLatestState) => [
+      ...previousTodosLatestState,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
   return (
     <div className="App">
-      <TodoList items={todos}/>
-      <NewTodo addHandler={addHandler}/>
+      <TodoList items={todos} />
+      <NewTodo addHandler={addHandler} />
     </div>
   );
 }
